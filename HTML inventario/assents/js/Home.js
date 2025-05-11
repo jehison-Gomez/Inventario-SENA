b02481e65fb3fcb92ca51978178f3ce721a94bd7
jehison-Gomez <jehisondavidgomezdelgado@gmail.com>
2025-05-11 10:10:30 -0500

    html,css y JS de modulo de home

---
+++ HTML inventario/assents/js/Home.js
@@ -0,0 +1,34 @@
+const menusItemsDropDown = document.querySelectorAll('.menu-item-dropdown');
+const sidebar = document.getElementById('sidebar');
+const menuBtn = document.getElementById('menu-btn');
+
+menuBtn.addEventListener('click',()=>{
+    sidebar.classList.toggle('minimize');
+});
+
+menusItemsDropDown.forEach((menuItem) =>{
+    menuItem.addEventListener('click',()=>{
+        const subMenu = menuItem.querySelector('.sub-menu');
+        const isActive = menuItem.classList.toggle('sub-menu-toggle');
+        if(subMenu){
+            if(isActive){
+                subMenu.style.height = `${subMenu.scrollHeight + 6}px`;
+                subMenu.style.padding = '0.2rem 0';
+            }else{
+                subMenu.style.height = '0';
+                subMenu.style.padding = '0';
+            }
+        }
+        menusItemsDropDown.forEach((item)=>{
+            if(item !== menuItem){
+                const otherSubmenu = item.querySelector('.sub-menu');
+                if(otherSubmenu){
+                    item.classList.remove('sub-menu-toggle');
+                    otherSubmenu.style.height = '0';
+                    otherSubmenu.style.padding = '0';
+                }
+            }
+        })
+    });
+});
+console.log('hola');
